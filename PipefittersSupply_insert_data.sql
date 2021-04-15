2ef086e34116d70c97b7733e0e6d18ed71d62e94
brthomasusa <brian.thomas@btechnical-consulting.com>
2021-04-15 16:48:28 -0500

    Initial commit

---
+++ PipefittersSupply_insert_data.sql
@@ -0,0 +1,5 @@
+INSERT INTO Sales.Customers
+    (CustomerID, CustomerName, AddressLine1, AddressLine2, City, [State], ZipCode, Telephone, CreditLimit, PrimaryContact)
+VALUES
+    (100001, 'Dunn Pluming', '2763 Cosgrove Road', '2nd Floor', 'West Haven', 'CT', '06516-1960', '203-913-2063', 6000, 'Bill Dunn')
+GO
