644cb22573dbaa593bac247c9a4dcbf912b9df27
Brian Thomas <brian.thomas@btechnical-consulting.com>
2021-04-14 22:35:56 -0500

    Just playin around

--- PipefittersSupply.sql
+++ PipefittersSupply.sql
@@ -2,3 +2,8 @@ CREATE DATABASE PipefittersSupply
 GO
 
 USE PipefittersSupply;
+
+SELECT type_desc, size * 8 / 1024 AS size_MB, physical_name
+FROM sys.master_files
+WHERE database_id = DB_ID('PipefittersSupply');
+
