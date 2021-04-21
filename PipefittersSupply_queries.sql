4f02b73356fc5fd1db4fc459e95c2d58613a5647
Brian Thomas <brian.thomas@btechnical-consulting.com>
2021-04-20 23:13:12 -0500

    Created function HumanResources.udf_GetAllEmployeeFullNames

--- PipefittersSupply_queries.sql
+++ PipefittersSupply_queries.sql
@@ -1,43 +1,54 @@
-CREATE OR ALTER FUNCTION Sales.udf_GetAllCustomers()
+-- CREATE OR ALTER FUNCTION Sales.udf_GetAllCustomers()
+-- RETURNS TABLE
+-- AS
+-- RETURN
+--     SELECT 
+--         cust.CustomerID, cust.CustomerName, cust.AddressLine1, cust.AddressLine2,
+--         cust.City + ', ' + cust.[State] + ' ' + cust.ZipCode AS CityStateZip,
+--         cust.Telephone, cust.CreditLimit, cust.PrimaryContact
+--     FROM Sales.Customers cust;
+-- GO
+
+-- CREATE OR ALTER FUNCTION Sales.udf_GetCustomerByID(
+--     @customerID INT
+-- )
+-- RETURNS TABLE
+-- AS
+-- RETURN
+--     SELECT 
+--         cust.CustomerID, cust.CustomerName, cust.AddressLine1, cust.AddressLine2,
+--         cust.City + ', ' + cust.[State] + ' ' + cust.ZipCode AS CityStateZip,
+--         cust.Telephone, cust.CreditLimit, cust.PrimaryContact
+--     FROM Sales.Customers cust
+--     WHERE cust.CustomerID = @customerID;
+-- GO
+
+-- CREATE OR ALTER FUNCTION Sales.udf_GetSalesOrderDetailsBySalesOrderID(
+--     @salesOrderID INT
+-- )
+-- RETURNS TABLE
+-- AS
+-- RETURN
+--     SELECT 
+--         sod.InventoryID, 
+--         inv.[Description], 
+--         sod.QuantityOrdered, 
+--         sod.UnitPrice AS Price,
+--         inv.ListPrice AS 'List Price',
+--         sod.QuantityOrdered * sod.UnitPrice AS Extension,
+--         sod.SalesOrderID
+--     FROM Sales.SalesOrderDetails sod
+--         INNER JOIN Sales.Inventory inv ON sod.InventoryID = inv.InventoryID
+--     WHERE sod.SalesOrderID = @salesOrderID
+-- GO
+
+CREATE OR ALTER FUNCTION HumanResources.udf_GetAllEmployeeFullNames()
 RETURNS TABLE
 AS
 RETURN
     SELECT 
-        cust.CustomerID, cust.CustomerName, cust.AddressLine1, cust.AddressLine2,
-        cust.City + ', ' + cust.[State] + ' ' + cust.ZipCode AS CityStateZip,
-        cust.Telephone, cust.CreditLimit, cust.PrimaryContact
-    FROM Sales.Customers cust;
+        emp.EmployeeID, emp.FirstName + ' ' + ISNULL(emp.MiddleInitial + ' ', '')  + emp.LastName AS 'Employee Name'
+    FROM HumanResources.Employees emp;
 GO
 
-CREATE OR ALTER FUNCTION Sales.udf_GetCustomerByID(
-    @customerID INT
-)
-RETURNS TABLE
-AS
-RETURN
-    SELECT 
-        cust.CustomerID, cust.CustomerName, cust.AddressLine1, cust.AddressLine2,
-        cust.City + ', ' + cust.[State] + ' ' + cust.ZipCode AS CityStateZip,
-        cust.Telephone, cust.CreditLimit, cust.PrimaryContact
-    FROM Sales.Customers cust
-    WHERE cust.CustomerID = @customerID;
-GO
 
-CREATE OR ALTER FUNCTION Sales.udf_GetSalesOrderDetailsBySalesOrderID(
-    @salesOrderID INT
-)
-RETURNS TABLE
-AS
-RETURN
-    SELECT 
-        sod.InventoryID, 
-        inv.[Description], 
-        sod.QuantityOrdered, 
-        sod.UnitPrice AS Price,
-        inv.ListPrice AS 'List Price',
-        sod.QuantityOrdered * sod.UnitPrice AS Extension,
-        sod.SalesOrderID
-    FROM Sales.SalesOrderDetails sod
-        INNER JOIN Sales.Inventory inv ON sod.InventoryID = inv.InventoryID
-    WHERE sod.SalesOrderID = @salesOrderID
-GO
