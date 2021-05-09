143c6ec6430209c9748eafe914d277218a0e0f78
Brian Thomas <brian.thomas@btechnical-consulting.com>
2021-05-09 11:08:24 -0500

    Created CashAccounts, CashDisbursements, CashReceipts and type tables in Finance schema

--- Pipefitters_tSQLt.sql
+++ Pipefitters_tSQLt.sql
@@ -1,3 +1,6 @@
+CREATE DATABASE Pipefitters_tSQLt
+GO
+
 USE Pipefitters_tSQLt;
 GO
 
@@ -46,6 +49,7 @@ CREATE TABLE HumanResources.Employees
   Exemptions int DEFAULT 0 NOT NULL,
   PayRate decimal(18, 2) CHECK (PayRate >= 7.50 AND PayRate <= 40.00) NOT NULL,
   StartDate datetime2(0) NOT NULL,
+  IsActive BIT DEFAULT 1 NOT NULL,
   CreatedDate datetime2(7) DEFAULT sysdatetime() NOT NULL,
   LastModifiedDate datetime2(7) NULL
 );
@@ -60,24 +64,24 @@ CREATE UNIQUE INDEX idx_Employees$LastName$FirstName$MiddlieInitial
 GO
 
 INSERT INTO HumanResources.Employees
-    (EmployeeID, EmployeeTypeID, LastName, FirstName, MiddleInitial, SSN, AddressLine1, City, [State], ZipCode, Telephone, MaritalStatus, Exemptions, PayRate, StartDate)
+    (EmployeeID, EmployeeTypeID, LastName, FirstName, MiddleInitial, SSN, AddressLine1, City, [State], ZipCode, Telephone, MaritalStatus, Exemptions, PayRate, StartDate, IsActive)
 VALUES
-    (101, 2, 'Sanchez', 'Ken', 'J', '123789999', '321 Tarrant Pl', 'Fort Worth', 'TX', '78965', '817-987-1234', 'M', 5, 40.00, '1998-12-02'),
-    (108, 2, 'Phide', 'Terri', 'M', '638912345', '3455 South Corinth Circle', 'Dallas', 'TX', '75224', '214-987-1234', 'M', 1, 28.00, '2014-09-22'),
-    (109, 2, 'Duffy', 'Terri', 'L', '999912345', '98 Reiger Ave', 'Dallas', 'TX', '75214', '214-987-1234', 'M', 2, 30.00, '2018-10-22'),
-    (111, 2, 'Goldberg', 'Jozef', 'P', '036889999', '6667 Melody Lane, Apt 2', 'Dallas', 'TX', '75231', '469-321-1234', 'S', 1, 29.00, '2013-02-28'),
-    (112, 2, 'Brown', 'Jamie', 'J', '123700009', '98777 Nigeria Town Rd', 'Arlington', 'TX', '78658', '817-555-5555', 'M', 2, 29.00, '2017-12-22'),
-    (113, 2, 'Bush', 'George', 'W', '325559874', '777 Ervay Street', 'Dallas', 'TX', '75208', '214-555-5555', 'M', 5, 30.00, '2016-10-19'),
-    (114, 1, 'Bushnell', 'Loretta', 'J', '370005409', '123 Main St', 'Flower Mound', 'TX', '78630', '817-555-5555', 'M', 2, 25.00, '2017-02-22'),
-    (115, 1, 'Jacknoff', 'Jorge', 'C', '325509345', '777 Ervay Street', 'Dallas', 'TX', '75208', '214-987-5555', 'M', 3, 25.00, '2014-06-19'),
-    (116, 3, 'Trump', 'Donald', 'J', '781287999', '1 Cowgirl Circle', 'Fort Worth', 'TX', '78965', '817-123-9874', 'M', 4, 8.00, '2003-07-02'),
-    (117, 3, 'Trump', 'Ivanka', 'Z', '256281432', '1 Cowgirl Circle', 'Fort Worth', 'TX', '78965', '817-123-9874', 'M', 2, 10.50, '2003-07-02'),
-    (118, 4, 'Thompson', 'Douglas', 'J', '371005409', '4338 Main Place', 'Rice', 'TX', '78430', '972-555-5555', 'S', 2, 17.00, '2017-02-22'),
-    (119, 4, 'Hernandez', 'Jesus', 'G', '323509345', '1254 34th St', 'Irving', 'TX', '75268', '972-987-5555', 'M', 3, 15.00, '2019-06-19'),
-    (120, 5, 'Doe', 'Jonny', 'A', '567882345', '32584 Collett Ln', 'Saginaw', 'TX', '78965', '817-123-9874', 'M', 4, 22.00, '2015-07-22'),
-    (121, 5, 'Smith', 'Samuel', 'P', '256200432', '34 Wensworth Drive', 'Roanocke', 'TX', '78965', '817-123-9874', 'M', 2, 22.50, '2018-03-14'),
-    (122, 6, 'Gomez', 'Roberto', 'J', '687005409', '4338 Main Place', 'Desoto', 'TX', '74430', '972-555-5555', 'S', 2, 17.00, '2017-02-22'),
-    (123, 6, 'Hernandez', 'George', 'G', '478509345', '65 Adelia Lane', 'Ricardson', 'TX', '75268', '972-987-5555', 'M', 3, 15.00, '2019-06-19')
+    (101, 2, 'Sanchez', 'Ken', 'J', '123789999', '321 Tarrant Pl', 'Fort Worth', 'TX', '78965', '817-987-1234', 'M', 5, 40.00, '1998-12-02', 1),
+    (108, 2, 'Phide', 'Terri', 'M', '638912345', '3455 South Corinth Circle', 'Dallas', 'TX', '75224', '214-987-1234', 'M', 1, 28.00, '2014-09-22', 1),
+    (109, 2, 'Duffy', 'Terri', 'L', '999912345', '98 Reiger Ave', 'Dallas', 'TX', '75214', '214-987-1234', 'M', 2, 30.00, '2018-10-22', 1),
+    (111, 2, 'Goldberg', 'Jozef', 'P', '036889999', '6667 Melody Lane, Apt 2', 'Dallas', 'TX', '75231', '469-321-1234', 'S', 1, 29.00, '2013-02-28', 1),
+    (112, 2, 'Brown', 'Jamie', 'J', '123700009', '98777 Nigeria Town Rd', 'Arlington', 'TX', '78658', '817-555-5555', 'M', 2, 29.00, '2017-12-22', 1),
+    (113, 2, 'Bush', 'George', 'W', '325559874', '777 Ervay Street', 'Dallas', 'TX', '75208', '214-555-5555', 'M', 5, 30.00, '2016-10-19', 1),
+    (114, 1, 'Bushnell', 'Loretta', 'J', '370005409', '123 Main St', 'Flower Mound', 'TX', '78630', '817-555-5555', 'M', 2, 25.00, '2017-02-22', 1),
+    (115, 1, 'Jacknoff', 'Jorge', 'C', '325509345', '777 Ervay Street', 'Dallas', 'TX', '75208', '214-987-5555', 'M', 3, 25.00, '2014-06-19', 1),
+    (116, 3, 'Trump', 'Donald', 'J', '781287999', '1 Cowgirl Circle', 'Fort Worth', 'TX', '78965', '817-123-9874', 'M', 4, 8.00, '2003-07-02', 1),
+    (117, 3, 'Trump', 'Ivanka', 'Z', '256281432', '1 Cowgirl Circle', 'Fort Worth', 'TX', '78965', '817-123-9874', 'M', 2, 10.50, '2003-07-02', 1),
+    (118, 4, 'Thompson', 'Douglas', 'J', '371005409', '4338 Main Place', 'Rice', 'TX', '78430', '972-555-5555', 'S', 2, 17.00, '2017-02-22', 1),
+    (119, 4, 'Hernandez', 'Jesus', 'G', '323509345', '1254 34th St', 'Irving', 'TX', '75268', '972-987-5555', 'M', 3, 15.00, '2019-06-19', 1),
+    (120, 5, 'Doe', 'Jonny', 'A', '567882345', '32584 Collett Ln', 'Saginaw', 'TX', '78965', '817-123-9874', 'M', 4, 22.00, '2015-07-22', 1),
+    (121, 5, 'Smith', 'Samuel', 'P', '256200432', '34 Wensworth Drive', 'Roanocke', 'TX', '78965', '817-123-9874', 'M', 2, 22.50, '2018-03-14', 1),
+    (122, 6, 'Gomez', 'Roberto', 'J', '687005409', '4338 Main Place', 'Desoto', 'TX', '74430', '972-555-5555', 'S', 2, 17.00, '2017-02-22', 1),
+    (123, 6, 'Hernandez', 'George', 'G', '478509345', '65 Adelia Lane', 'Ricardson', 'TX', '75268', '972-987-5555', 'M', 3, 15.00, '2019-06-19', 1)
 GO
 
 CREATE TABLE HumanResources.ExemptionLookUps
@@ -217,3 +221,141 @@ VALUES
     (31, 122, 113, '2021-02-28', 160, 7),
     (32, 123, 113, '2021-02-28', 160, 0)    
 GO
+
+CREATE TABLE Finance.CashAccounts
+(
+  CashAccountID int PRIMARY KEY CLUSTERED,
+  AccountDescription nvarchar(30) NOT NULL UNIQUE,
+  BankName nvarchar(30) NOT NULL,
+  DateEstablished datetime2(0) NOT NULL,
+  CreatedDate datetime2(7) DEFAULT sysdatetime() NOT NULL,
+  LastModifiedDate datetime2(7) NULL
+)
+GO
+
+INSERT INTO Finance.CashAccounts
+    (CashAccountID, AccountDescription, BankName, DateEstablished)
+VALUES
+    (101, 'Primary Checking Account', 'First Bank', '2020-09-03'),
+    (102, 'Payroll Account', 'First Bank', '2020-09-03'),
+    (103, 'Financing Proceeds', 'First Bank', '2020-12-20')
+GO
+
+CREATE TABLE Finance.CashDisbursementTypes
+(
+  CashDisbursementTypeID INT PRIMARY KEY CLUSTERED,
+  EventTypeName NVARCHAR(25) NOT NULL  UNIQUE,
+  PayeeTypeName NVARCHAR(25) NOT NULL  UNIQUE
+)
+GO
+
+INSERT INTO Finance.CashDisbursementTypes
+    (CashDisbursementTypeID, EventTypeName, PayeeTypeName)
+VALUES
+    (1, 'Labor Acquisition', 'Employee'),
+    (2, 'Purchasing', 'Vendor'),
+    (3, 'Loan Payment', 'Creditor'),
+    (4, 'Dividend Payments', 'Stockholder')
+GO
+
+CREATE TABLE Finance.CashDisbursements
+(
+  CashDisbursementID INT PRIMARY KEY CLUSTERED,
+  CheckNumber NVARCHAR(15) NOT NULL,
+  CashAccountID INT NOT NULL REFERENCES Finance.CashAccounts(CashAccountID),
+  CashDisbursementTypeID INT NOT NULL REFERENCES Finance.CashDisbursementTypes(CashDisbursementTypeID),
+  PayeeID INT NOT NULL,
+  EmployeeID INT NOT NULL REFERENCES HumanResources.Employees (EmployeeID),
+  EventID INT NOT NULL,
+  DisbursementAmount DECIMAL(18,2) NOT NULL,
+  DisbursementDate DATETIME2(0) NOT NULL,
+  CreatedDate datetime2(7) DEFAULT sysdatetime() NOT NULL,
+  LastModifiedDate datetime2(7) NULL
+)
+GO
+
+CREATE INDEX IDX_CashDisbursements$CashAccountID
+  ON Finance.CashDisbursements (CashAccountID)
+GO
+
+CREATE INDEX IDX_CashDisbursements$CashDisbursementTypeID
+  ON Finance.CashDisbursements (CashDisbursementTypeID)
+GO
+
+CREATE INDEX IDX_CashDisbursements$EmployeeID
+  ON Finance.CashDisbursements (EmployeeID)
+GO
+
+CREATE INDEX IDX_CashDisbursements$PayeeID
+  ON Finance.CashDisbursements (PayeeID)
+GO
+
+CREATE INDEX IDX_CashDisbursements$EventID
+  ON Finance.CashDisbursements (EventID)
+GO
+
+CREATE UNIQUE INDEX IDX_CashDisbursement$CheckNumber$CashAccountID 
+  ON Finance.CashDisbursements (CheckNumber, CashAccountID)
+GO
+
+CREATE TABLE Finance.CashReceiptTypes
+(
+  CashReceiptTypeID INT PRIMARY KEY CLUSTERED,
+  EventTypeName NVARCHAR(25) NOT NULL,
+  PayeeTypeName NVARCHAR(25) NOT NULL
+)
+GO
+
+CREATE UNIQUE INDEX idx_CashReceiptType$EventTypeName
+    ON Finance.CashReceiptTypes (EventTypeName)
+GO
+
+CREATE UNIQUE INDEX idx_CashReceiptType$PayeeTypeName
+    ON Finance.CashReceiptTypes (PayeeTypeName)
+GO
+
+INSERT INTO Finance.CashReceiptTypes
+    (CashReceiptTypeID, EventTypeName, PayeeTypeName)
+VALUES
+    (1, 'Sale', 'Customer'),
+    (2, 'Debt Issue', 'Creditor'),
+    (3, 'Stock Issue', 'Stockholder')
+GO
+
+CREATE TABLE Finance.CashReceipts
+(
+  CashReceiptID int PRIMARY KEY CLUSTERED,
+  CashReceiptTypeID INT NOT NULL REFERENCES Finance.CashReceiptTypes (CashReceiptTypeID),
+  EventID int NOT NULL,
+  CashAccountID int NOT NULL REFERENCES Finance.CashAccounts (CashAccountID),
+  PayeeID int NOT NULL,
+  EmployeeID int NOT NULL REFERENCES HumanResources.Employees (EmployeeID),
+  RemittanceAdviceID nvarchar(25) NOT NULL,
+  CashReceiptDate datetime2(0) NOT NULL,
+  CashReceiptAmount decimal(18, 2) CHECK (CashReceiptAmount >= 0) NOT NULL,
+  PayorCheckNumber nvarchar(15) NOT NULL,
+  CreatedDate datetime2(7) DEFAULT sysdatetime() NOT NULL,
+  LastModifiedDate datetime2(7) NULL
+)
+GO
+
+CREATE INDEX idx_CashReceipts$CashReceiptTypeID 
+  ON Finance.CashReceipts (CashReceiptTypeID);
+GO
+
+CREATE INDEX idx_CashReceipts$CashAccountID 
+  ON Finance.CashReceipts (CashAccountID);
+GO
+
+CREATE INDEX idx_CashReceipts$EmployeeID 
+  ON Finance.CashReceipts (EmployeeID)
+GO
+
+CREATE INDEX idx_CashReceipts$EventID 
+  ON Finance.CashReceipts (EventID);
+GO
+
+CREATE INDEX idx_CashReceipts$PayeeID 
+  ON Finance.CashReceipts (PayeeID)
+GO
+
