c1a31c1da6738b715033069dc03e5fa39eeb3ac6
brthomasusa <brian.thomas@btechnical-consulting.com>
2021-05-16 11:20:11 -0500

    Added AddressLine1 and AddressLine2 value object classes

---
+++ src/PipefittersSupply.Domain/Common/AddressLine1.cs
@@ -0,0 +1,27 @@
+using System;
+using PipefittersSupply.Framework;
+
+namespace PipefittersSupply.Domain.Common
+{
+    public class AddressLine1 : Value<AddressLine1>
+    {
+        private readonly string _value;
+
+        private AddressLine1(string value)
+        {
+            if (string.IsNullOrEmpty(value))
+            {
+                throw new ArgumentNullException("The first address line is required.", nameof(value));
+            }
+
+            if (value.Length > 25)
+            {
+                throw new ArgumentOutOfRangeException("Address line can not be longer than 30 characters.", nameof(value));
+            }
+
+            _value = value;
+        }
+
+        public static AddressLine1 FromString(string address) => new AddressLine1(address);
+    }
+}
---
+++ src/PipefittersSupply.Domain/Common/AddressLine2.cs
@@ -0,0 +1,27 @@
+using System;
+using PipefittersSupply.Framework;
+
+namespace PipefittersSupply.Domain.Common
+{
+    public class AddressLine2 : Value<AddressLine2>
+    {
+        private readonly string _value;
+
+        private AddressLine2(string value)
+        {
+            if (string.IsNullOrEmpty(value))
+            {
+                throw new ArgumentNullException("The first address line is required.", nameof(value));
+            }
+
+            if (value.Length > 25)
+            {
+                throw new ArgumentOutOfRangeException("Address line can not be longer than 30 characters.", nameof(value));
+            }
+
+            _value = value;
+        }
+
+        public static AddressLine2 FromString(string address) => new AddressLine2(address);
+    }
+}
