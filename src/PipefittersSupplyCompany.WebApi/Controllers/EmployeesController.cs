2f598f366022eeded7c1f86fed8fea2c858fca68
brthomasusa <brian.thomas@btechnical-consulting.com>
2021-09-06 18:00:49 -0500

    Only send paging info as query parameters

--- src/PipefittersSupplyCompany.WebApi/Controllers/EmployeesController.cs
+++ src/PipefittersSupplyCompany.WebApi/Controllers/EmployeesController.cs
@@ -1,3 +1,4 @@
+using System;
 using Microsoft.AspNetCore.Mvc;
 using System.Threading.Tasks;
 using PipefittersSupplyCompany.Infrastructure.Interfaces;
@@ -32,9 +33,14 @@ namespace PipefittersSupplyCompany.WebApi.Controllers
 
         [HttpGet]
         [Route("list")]
-        public async Task<IActionResult> Get([FromQuery] PagingParameters pagingParams)
+        public async Task<IActionResult> GetEmployees([FromQuery] PagingParameters pagingParams)
         {
-            GetEmployees queryParams = new GetEmployees { Page = pagingParams.Page ?? 1, PageSize = pagingParams.PageSize ?? 4 };
+            GetEmployees queryParams =
+                new GetEmployees
+                {
+                    Page = pagingParams.Page ?? 1,
+                    PageSize = pagingParams.PageSize ?? 4
+                };
 
             return await RequestHandler.HandleQuery
                         (
@@ -44,31 +50,59 @@ namespace PipefittersSupplyCompany.WebApi.Controllers
         }
 
         [HttpGet]
-        [Route("supervisedby")]
-        public async Task<IActionResult> Get([FromQuery] GetEmployeesSupervisedBy queryParams) =>
-            await RequestHandler.HandleQuery
+        [Route("supervisedby/{supervisorId:Guid}")]
+        public async Task<IActionResult> GetSupervisedBy(Guid supervisorId, [FromQuery] PagingParameters pagingParams)
+        {
+            GetEmployeesSupervisedBy queryParams =
+                new GetEmployeesSupervisedBy
+                {
+                    SupervisorID = supervisorId,
+                    Page = pagingParams.Page ?? 1,
+                    PageSize = pagingParams.PageSize ?? 4
+                };
+
+            return await RequestHandler.HandleQuery
             (
                 () => _employeeQrySvc.Query(queryParams),
                 _logger
             );
+        }
 
         [HttpGet]
-        [Route("employeesofrole")]
-        public async Task<IActionResult> Get([FromQuery] GetEmployeesOfRole queryParams) =>
-            await RequestHandler.HandleQuery
+        [Route("employeesofrole/{roleId:Guid}")]
+        public async Task<IActionResult> GetRoleMembers(Guid roleId, [FromQuery] PagingParameters pagingParams)
+        {
+            GetEmployeesOfRole queryParams =
+                new GetEmployeesOfRole
+                {
+                    RoleID = roleId,
+                    Page = pagingParams.Page ?? 1,
+                    PageSize = pagingParams.PageSize ?? 4
+                };
+
+            return await RequestHandler.HandleQuery
             (
                 () => _employeeQrySvc.Query(queryParams),
                 _logger
             );
+        }
 
         [HttpGet]
-        [Route("details")]
-        public async Task<IActionResult> Get([FromQuery] GetEmployee queryParams) =>
-            await RequestHandler.HandleQuery
+        [Route("details/{employeeId:Guid}")]
+        public async Task<IActionResult> GetEmployee(Guid employeeId)
+        {
+            GetEmployee queryParams =
+                new GetEmployee
+                {
+                    EmployeeID = employeeId
+                };
+
+            return await RequestHandler.HandleQuery
             (
                 () => _employeeQrySvc.Query(queryParams),
                 _logger
             );
+        }
 
         [HttpPost]
         public async Task<IActionResult> Post([FromBody] V1.CreateEmployeeInfo command) =>
