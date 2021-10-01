1bd824e8be484efe49d483e003b8ccc6049318dc
brthomasusa <brian.thomas@btechnical-consulting.com>
2021-10-01 14:02:14 -0500

    CreateEmployeeAddress action method passed integration testing

--- tests/PipefittersSupplyCompany.IntegrationTests/HumanResources/Controllers/EmployeesControllerTests.cs
+++ tests/PipefittersSupplyCompany.IntegrationTests/HumanResources/Controllers/EmployeesControllerTests.cs
@@ -7,6 +7,7 @@ using System.Net;
 using System.Net.Http;
 using Newtonsoft.Json;
 using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.Net.Http.Headers;
 using Xunit;
 using PipefittersSupplyCompany.WebApi;
 using PipefittersSupplyCompany.Infrastructure.Application.Queries;
@@ -20,12 +21,13 @@ namespace PipefittersSupplyCompany.IntegrationTests.HumanResources.Controllers
     public class EmployeesControllerTests : IntegrationTestBaseEfCore, IClassFixture<WebApplicationFactory<PipefittersSupplyCompany.WebApi.Startup>>
     {
         private HttpClient _client;
-        private readonly string _serviceAddress = "https://localhost:5001/";
+        private readonly string _serviceAddress = "https://localhost:5011/";
         private readonly string _rootAddress = "api/1.0/employees";
 
         public EmployeesControllerTests(WebApplicationFactory<PipefittersSupplyCompany.WebApi.Startup> factory)
         {
             _client = factory.CreateClient();
+            _client.DefaultRequestHeaders.Add("Accept", "*/*");
             TestDataInitialization.InitializeData(_dbContext);
         }
 
@@ -153,5 +155,59 @@ namespace PipefittersSupplyCompany.IntegrationTests.HumanResources.Controllers
 
             Assert.True(response.IsSuccessStatusCode);
         }
+
+        [Theory]
+        [InlineData("4b900a74-e2d9-4837-b9a4-9e828752716e", 2)]
+        [InlineData("5c60f693-bef5-e011-a485-80ee7300c695", 1)]
+        [InlineData("660bb318-649e-470d-9d2b-693bfb0b2744", 1)]
+        [InlineData("9f7b902d-566c-4db6-b07b-716dd4e04340", 1)]
+        [InlineData("aedc617c-d035-4213-b55a-dae5cdfca366", 1)]
+        [InlineData("0cf9de54-c2ca-417e-827c-a5b87be2d788", 1)]
+        [InlineData("e716ac28-e354-4d8d-94e4-ec51f08b1af8", 1)]
+        [InlineData("604536a1-e734-49c4-96b3-9dfef7417f9a", 1)]
+        public async Task GET_PagedList_All_AddressesForOneEmployee(Guid employeeId, int numberOfAddresses)
+        {
+            var pagingParams = new PagingParameters { Page = 1, PageSize = 10 };
+
+            var response = await _client.GetAsync($"{_serviceAddress}{_rootAddress}/{employeeId}/addresses?Page={pagingParams.Page}&PageSize={pagingParams.PageSize}");
+
+            Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+
+            var jsonResponse = await response.Content.ReadAsStringAsync();
+            var employeeListItems = JsonConvert.DeserializeObject<List<EmployeeAddressListItem>>(jsonResponse);
+
+            Assert.Equal(numberOfAddresses, employeeListItems.Count);
+        }
+
+        [Fact]
+        public async Task ShouldCreate_EmployeeAddressInfo_UsingEmployeeController()
+        {
+            Guid employeeId = new Guid("604536a1-e734-49c4-96b3-9dfef7417f9a");
+            var command = new CreateEmployeeAddressInfo
+            {
+                EmployeeId = employeeId,
+                AddressLine1 = "5558 Reiger Street",
+                AddressLine2 = "3rd Floor",
+                City = "Dallas",
+                StateCode = "TX",
+                Zipcode = "75214"
+            };
+
+            string jsonEmployee = JsonConvert.SerializeObject(command);
+            HttpContent content = new StringContent(jsonEmployee, Encoding.UTF8, "application/json");
+            var response = await _client.PostAsync($"{_serviceAddress}{_rootAddress}/{employeeId}/createemployeeaddressinfo", content);
+
+            Assert.True(response.IsSuccessStatusCode);
+        }
+
+
+
+
+
+
+
+
+
+
     }
 }
